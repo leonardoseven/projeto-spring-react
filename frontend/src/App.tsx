1be13828fa92f57f2feabbab08782d5291e4757d
Leonardo dos Santos <lsantos@filah.com.br>
2022-06-16 13:38:30 -0300

    ADDED static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import NavBar from "components/navbar"
 import Footer from "components/Footer"
 import DataTable from "components/DataTable";
+import BarChart from "components/BarChart";
+import DonutChart from "components/DonutChart";
 
 
 function App() {
@@ -8,11 +10,24 @@ function App() {
     <>
       <NavBar />
       <div className="App container">
-        <h1 className="text-primary">Ola mundo</h1>
+        <h1 className="text-primary">Dashboard de Vendas</h1>
+        <div className="row px-3">
+          <div className="col-lg-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-lg-6">
+            <h5 className="text-center text-secondary">Todoas as Vendas</h5>
+            <DonutChart />
+          </div>
+        </div>
+        <div className="py-3">
+          <h1 className="text-primary">Dashboard de Vendas</h1>
+        </div>
 
-        <DataTable/>
+        <DataTable />
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 }
